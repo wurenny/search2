0fdea525268abf055997f007b8a5492cd7da0163
wurenny <wurenny@gmail.com>
2025-03-24 19:09:19 +0800

    v2.1.1 release to webstore

--- src/inc/comm.js
+++ src/inc/comm.js
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *
- * Copyright (c) 2014-2015, wurenny@gmail.com, All rights reserved
+ * Copyright (c) 2014-2025, wurenny@gmail.com, All rights reserved
  *
  * IDENTIFICATION
  *     inc/comm.js
